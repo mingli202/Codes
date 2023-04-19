9c2625ac773485da06f45f551a8c95f522b91543
Vincent Liu <vincentmingli@gmail.com>
2023-04-19 10:05:24 -0400

    April 19 2023

--- C++/snake_game/snake.cpp
+++ C++/snake_game/snake.cpp
@@ -14,6 +14,12 @@
 #include <SDL2/SDL.h>
 #include <SDL2_ttf/SDL_ttf.h>
 
+typedef struct Food{
+    int x;
+    int y;
+    std::string type;
+}Food;
+
 class Player{
     public:
     typedef struct info{
@@ -25,8 +31,12 @@ class Player{
         int dy;
     }info;
 
-    std::vector<info> snake = {{340, 340, 0, 0, 0, 0}};
+    std::vector<info> snake = {};
     info tmp;
+
+    int red = 255;
+    int green = 255;
+    int blue = 255;
 
     int RECT_SIZE = 20;
 
@@ -51,7 +61,7 @@ class Player{
         }
 
         SDL_Rect rect;
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_SetRenderDrawColor(renderer, red, green, blue, 255);
 
         rect = {snake[0].x, snake[0].y, RECT_SIZE, RECT_SIZE};
         SDL_RenderFillRect(renderer, &rect);
@@ -66,16 +76,22 @@ class Player{
         }
     }
 
-    void on_food_collision(std::vector<info>& snake, SDL_Point& food){
+    void on_food_collision(std::vector<info>& snake, std::vector<Food>& food){
 
-        if (snake[0].x == food.x && snake[0].y == food.y){
-            srand((unsigned) time(NULL));
-            food.x = rand() % 35 * 20;
-            food.y = rand() % 35 * 20;
+        for (Food& i : food){
+            if (snake[0].x == i.x && snake[0].y == i.y){
+                int LAST = snake.size() - 1;
 
-            tmp.x = snake[-1].x_previous;
-            tmp.y = snake[-1].y_previous;
-            snake.push_back(tmp);
+                srand((unsigned) time(NULL));
+                i.x = rand() % 35 * 20;
+                i.y = rand() % 35 * 20;
+
+                tmp.x = snake[LAST].x_previous;
+                tmp.y = snake[LAST].y_previous;
+                snake.push_back(tmp);
+
+                break;
+            }
         }
     }
 
@@ -88,7 +104,20 @@ class Player{
         return false;
     }
 
+    int player_collision_on_other(std::vector<info>& p1_snake, std::vector<info>& p2_snake){
+        if (p1_snake[0].x == p2_snake[0].x && p1_snake[0].y == p2_snake[0].y){
+            return 2;
+        }
+
+        for (int i = 0; i < p2_snake.size() - 1; i++){
+            if (p1_snake[0].x == p2_snake[i].x_previous && p1_snake[0].y == p2_snake[i].y_previous){
+                return 1;
+            }
+        }
+        return 0;
+    }
 };
+
 
 class Text : public Player{
     public:
@@ -138,13 +167,26 @@ class Text : public Player{
     void display_score(SDL_Renderer* renderer, TTF_Font* font, SDL_Surface* surface, SDL_Texture* texture, std::vector<info>& snake){
         std::string score = "Score: " + std::to_string(snake.size() - 1);
 
-        surface = TTF_RenderText_Blended_Wrapped(font, score.c_str(), WHITE, 0); // white text
+        surface = TTF_RenderText_Solid(font, score.c_str(), WHITE); // white text
         texture = SDL_CreateTextureFromSurface(renderer, surface);
         SDL_FreeSurface(surface);
         int texW = 0;
         int texH = 0;
         SDL_QueryTexture(texture, NULL, NULL, &texW, &texH);
         SDL_Rect dstrect = {10, 10, texW, texH};
+        SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+    }
+
+    void display_second_player_score(SDL_Renderer* renderer, TTF_Font* font, SDL_Surface* surface, SDL_Texture* texture, std::vector<info>& snake){
+        std::string score = "Score: " + std::to_string(snake.size() - 1);
+
+        surface = TTF_RenderText_Solid(font, score.c_str(), WHITE); // white text
+        texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_FreeSurface(surface);
+        int texW = 0;
+        int texH = 0;
+        SDL_QueryTexture(texture, NULL, NULL, &texW, &texH);
+        SDL_Rect dstrect = {600, 10, texW, texH};
         SDL_RenderCopy(renderer, texture, NULL, &dstrect);
     }
 
@@ -157,26 +199,81 @@ class Text : public Player{
         int texW = 0;
         int texH = 0;
         SDL_QueryTexture(texture, NULL, NULL, &texW, &texH);
-        SDL_Rect dstrect = {200, 300, texW, texH};
+        int center_x = (700 - texW) / 2;
+        int center_y = (700 - texH) / 2;
+        SDL_Rect dstrect = {center_x, center_y, texW, texH};
+        SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+    }
+
+    void display_winner_screen(SDL_Renderer* renderer, TTF_Font* font, SDL_Surface* surface, SDL_Texture* texture, int win){
+        std::string winner;
+        std::string win_message;
+        if (win == -1){
+            win_message = "Draw";
+        }
+        else{
+            winner = (win == 1) ? "Green" : "Blue";
+            win_message = winner + " wins!";
+        }
+        int texW = 0;
+        int texH = 0;
+        int center_x;
+        int center_y;
+        SDL_Rect dstrect;
+
+        surface = TTF_RenderText_Solid(font, win_message.c_str(), BLACK); // black text, winner
+        texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_FreeSurface(surface);
+        SDL_QueryTexture(texture, NULL, NULL, &texW, &texH);
+        center_x = (700 - texW) / 2;
+        center_y = (700 - texH) / 2;
+        dstrect = {center_x, center_y, texW, texH};
+        SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+
+        surface = TTF_RenderText_Solid(font, "Press Space to play again", BLACK); // black text
+        texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_FreeSurface(surface);
+        SDL_QueryTexture(texture, NULL, NULL, &texW, &texH);
+        center_x = (700 - texW) / 2;
+        center_y = (700 + texH) / 2;
+        dstrect = {center_x, center_y, texW, texH};
         SDL_RenderCopy(renderer, texture, NULL, &dstrect);
     }
 };
 
 
-void bg(SDL_Renderer* renderer, int bg_color){
+void bg(SDL_Renderer* renderer, const int bg_color){
     const int WINDOW_SIZE = 700;
     SDL_SetRenderDrawColor(renderer, bg_color, bg_color, bg_color, 255); // black
     SDL_Rect backgound = {0, 0, WINDOW_SIZE, WINDOW_SIZE};
     SDL_RenderFillRect(renderer, &backgound);
 }
 
-void draw_food(SDL_Renderer* renderer, SDL_Point food){
+void draw_food(SDL_Renderer* renderer, const std::vector<Food> food){
     // food
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // red
-    SDL_Rect food_rect = {food.x, food.y, 20, 20};
-    SDL_RenderFillRect(renderer, &food_rect);
+    for (Food i : food){
+        SDL_Rect food_rect = {i.x, i.y, 20, 20};
+        SDL_RenderFillRect(renderer, &food_rect);
+    }
 }
 
+void pause_screen(int& progress, int& previous){
+    bool unpause = false;
+    while (!unpause){
+        SDL_Event Pause;
+        while (SDL_PollEvent(&Pause)){
+            switch(Pause.type){
+                case (SDL_KEYDOWN):
+                    if (Pause.key.keysym.sym == SDLK_ESCAPE){
+                        progress = previous;
+                        unpause = true;
+                        break;
+                    }
+            }
+        }
+    }
+}
 
 int main(){
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -210,10 +307,12 @@ int main(){
     int bg_color = 255; // white
     int progress = 0;
     SDL_Point MyMouse;
+    int win = 0;
+    int previous;
 
     // food
     srand((unsigned) time(NULL));
-    SDL_Point food = {rand() % 35 * 20, rand() % 35 * 20};
+    std::vector<Food> food;
 
     // main loop
     bool quit = false;
@@ -235,15 +334,40 @@ int main(){
                 case (SDL_MOUSEBUTTONDOWN):
                     if (p1_text.color.r == p1_text.RED.r && progress == 0){
                         progress = 1;
+
+                        p1.tmp = {340, 340, 0, 0, 0, 0};
+                        p1.snake.push_back(p1.tmp);
+                        bg_color = 0;
+
+                        food = {{rand() % 35 * 20, rand() % 35 * 20}};
                     }
                     else if (p2_text.color.r == p2_text.RED.r && progress == 0){
                         progress = 2;
+
+                        p1.red = 200;
+                        p1.blue = 200;
+                        p1.tmp = {480, 340, 0, 0, 0, 0};
+                        p1.snake.push_back(p1.tmp);
+
+                        p2.red = 200;
+                        p2.green = 200;
+                        p2.tmp = {200, 340, 0, 0, 0, 0};
+                        p2.snake.push_back(p2.tmp);
+                        bg_color = 0;
+
+                        food = {{rand() % 35 * 20, rand() % 35 * 20}, {rand() % 35 * 20, rand() % 35 * 20}};
+        
                     }
-                    bg_color = 0;
                     break;
                 
                 case (SDL_KEYDOWN):
-                    if (progress == 1){
+                    if (event.key.keysym.sym == SDLK_ESCAPE && progress != 3){
+                        previous = progress;
+                        progress = 3;
+                        break;
+                    }
+
+                    if (progress == 1 || progress == 2){
                         if (event.key.keysym.sym == SDLK_LEFT){
                             p1.snake[0].dx = -1;
                             p1.snake[0].dy = 0;
@@ -252,7 +376,7 @@ int main(){
                         else if (event.key.keysym.sym == SDLK_RIGHT){
                             p1.snake[0].dx = 1;
                             p1.snake[0].dy = 0; 
-                            break;  
+                            break;
                         }
                         else if (event.key.keysym.sym == SDLK_UP){
                             p1.snake[0].dx = 0;
@@ -265,8 +389,31 @@ int main(){
                             break;
                         }
                     }
+
+                    if (progress == 2){
+                        if (event.key.keysym.sym == SDLK_a){
+                            p2.snake[0].dx = -1;
+                            p2.snake[0].dy = 0;
+                            break;
+                        }
+                        else if (event.key.keysym.sym == SDLK_d){
+                            p2.snake[0].dx = 1;
+                            p2.snake[0].dy = 0; 
+                            break;
+                        }
+                        else if (event.key.keysym.sym == SDLK_w){
+                            p2.snake[0].dx = 0;
+                            p2.snake[0].dy = -1;
+                            break;
+                        }
+                        else if (event.key.keysym.sym == SDLK_s){
+                            p2.snake[0].dx = 0;
+                            p2.snake[0].dy = 1;
+                            break;
+                        }
+                    }
                         
-                    if (progress == -1){
+                    if (progress == -1 || progress == -2){
                         if (event.key.keysym.sym == SDLK_SPACE){
                             TTF_CloseFont(font);
                             SDL_DestroyTexture(texture);
@@ -293,6 +440,7 @@ int main(){
                 p2_text.display_player_text(renderer, font, surface, texture, MyMouse, 1);
                 break;
                 
+            // single player
             case 1:
                 p1_text.display_score(renderer, font, surface, texture, p1.snake);
                 p1.display_player(renderer, p1.snake);
@@ -302,14 +450,71 @@ int main(){
                 if(p1.on_self_collision(p1.snake)){
                     progress = -1;
                     bg_color = 255;
+
                 }
                 break;
 
+            // two players
             case 2:
+                p1.display_player(renderer, p1.snake);
+                p1.on_food_collision(p1.snake, food);
+
+                p2.display_player(renderer, p2.snake);
+                p2.on_food_collision(p2.snake, food);
+                
+                draw_food(renderer, food);
+
+                // if (p1.on_self_collision(p1.snake)){
+                //     progress = -2;
+                //     bg_color = 255;
+                //     win = 2;
+                //     break;
+                // }
+                // if (p2.on_self_collision(p2.snake)){
+                //     progress = -2;
+                //     bg_color = 255;
+                //     win = 1;
+                //     break;
+                // }
+
+                // Draw
+                if (p1.player_collision_on_other(p1.snake, p2.snake) == 2){
+                    progress = -2;
+                    bg_color = 255;
+                    win = -1;
+                    break;
+                }
+
+                // p2 wins
+                if (p1.player_collision_on_other(p1.snake, p2.snake) == 1){
+                    progress = -2;
+                    bg_color = 255;
+                    win = 2;
+                    break;
+                }
+
+                // p1 wins
+                if (p2.player_collision_on_other(p2.snake, p1.snake) == 1){
+                    progress = -2;
+                    bg_color = 255;
+                    win = 1;
+                    break;
+                }
+
+
                 break;
-            
+
             case -1:
                 p1_text.display_death_screen(renderer, font, surface, texture, p1.snake);
+                break;
+
+            case -2:
+                p1_text.display_winner_screen(renderer, font, surface, texture, win);
+                break;
+
+            // pause screen
+            case 3:
+                pause_screen(progress, previous);
                 break;
 
         }
---
+++ C++/snake_game/test.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <limits>
+#include <random>
+#include <SDL2/SDL.h>
+#include <SDL2_ttf/SDL_ttf.h>
+
+typedef struct myStruct{
+    int x;
+    int y;
+    std::string type;
+}myStruct;
+
+void fibonacci_sequence(int size_of_sequence){
+    int a = 0;
+    int b = 1;
+    int c;
+
+    for(int i = 0; i < size_of_sequence; i++){
+        c = a + b;
+        a = b;
+        b = c;
+        std::cout << c << " ";
+    }
+}
+
+void collatz_sequence(int starting_number){
+    int number = starting_number;
+
+    while(number != 1){
+        if(number % 2 == 0){
+            number = number / 2;
+        }
+    }
+}
+
+int main(){
+
+    std::vector<int> myVect = {1, 2, 3, 4, 6, 7, 8};
+    std::cout << myVect[myVect.size() - 1];
+
+    std::cout << "\n";
+
+    std::vector<myStruct> Vector_of_Struct = {
+        {1, 2, "hello"},
+        {5, 3, "this is a test"}
+    };
+
+    int my_x = 5;
+    int my_y = 3;
+
+    auto lamdba_test = [my_x, my_y](myStruct s){
+        return (s.x == my_x &&  s.y == my_y);
+    };
+
+    std::cout << std::any_of(Vector_of_Struct.begin(), Vector_of_Struct.end(), lamdba_test);
+
+    std::cout << "\n";
+
+    std::vector<std::string> teamates = {"Ming Li, Qian, Katy, Sophia"};
+
+    
+
+    return 0;
+}
