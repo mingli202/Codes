fb815b6770557e363fe55d74dcb9c6719bf8e29c
mingli202 <vincentmingli@gmail.com>
2025-10-16 16:25:46 -0400

    idk man

--- Python/helloworld.py
+++ Python/helloworld.py
@@ -1,39 +1,54 @@
 import math
-
-n = 3
-
-
-def gen(i) -> list[int]:
-    k = 2**n
-
-    out = []
-
-    while k > 1:
-        out.append(math.floor(i % k / (k / 2)))
-        k /= 2
-
-    return out
+from dataclasses import dataclass
+from typing import Self
 
 
-out = [gen(i) for i in range(0, 2**n)]
+@dataclass
+class Node:
+    val: int
+    left: Self | None = None
+    right: Self | None = None
 
 
-def f(x: int, y: int, z: int) -> int:
-    return int(x and (not z or not y))
+class BST:
+    root: Node | None = None
+
+    def __init__(self, arr: list[int] = []):
+        for val in arr:
+            self.add(val)
+
+    def add(self, val: int):
+        node = self.root
+
+        if node is None:
+            self.root = Node(val)
+            return
+
+        while True:
+            if val < node.val:
+                if node.left is None:
+                    node.left = Node(val)
+                    return
+                else:
+                    node = node.left
+
+            else:
+                if node.right is None:
+                    node.right = Node(val)
+                    return
+                else:
+                    node = node.right
+
+    def to_arr(self) -> list[int]:
+        def traverse(node: Node | None) -> list[int]:
+            if node is None:
+                return []
+
+            return traverse(node.left) + [node.val] + traverse(node.right)
+
+        return traverse(self.root)
 
 
-for x, y, z in out:
-    print(f(x, y, z))
-
-
-a = {i for i in range(-4, 8)}
-b = {i for i in range(3, 12)}
-
-c = a
-print(c)
-print(len(c))
-
-n = 12
-
-for k in range(0, n + 1):
-    print(f"{k}!/({n}!*{n - k}!)")
+if __name__ == "__main__":
+    tree = BST([1, 5, 4, 3, 2, 6, 7, 8, 9, 10])
+    print(tree.to_arr())
--- Python/scrap/main.py
+++
@@ -1,5 +0,0 @@
-import random
-
-n = random.randint(0, 5)
-
-print(n)
