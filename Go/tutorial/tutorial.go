b5e116ea74c5bc68528c79389948e6401db22ee7
mingli202 <vincentmingli@gmail.com>
2026-02-05 21:48:27 -0500

    hmm most interesting indeed

--- Go/tutorial/tutorial.go
+++ Go/tutorial/tutorial.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello world this is go!")
+	messages := make(chan string)
+	var wg sync.WaitGroup
+
+	for i := range make([]bool, 10) {
+		wg.Go(func() {
+			time.Sleep(time.Second * time.Duration(rand.Float64()*5))
+			messages <- fmt.Sprintf("goroutine from %v!", i)
+		})
+		time.Sleep(time.Millisecond * 100)
+	}
+
+	go func() {
+		wg.Wait()
+		close(messages)
+	}()
+
+	for m := range messages {
+		fmt.Println(m)
+		time.Sleep(time.Millisecond * 100)
+	}
 }
